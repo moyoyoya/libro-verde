384612ac3fc90198707919e4d4e3475903affc40
unknown <ulises.moya@gmail.com>
2021-11-02 01:24:59 -0600

    will add everything eventually

---
+++ src/views/Mapa.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <h1  class="title is-2" >Tú guía de precios</h1>
+    <p class="title is-4">Libro verde es una guía de valores para la consulta fácil</p>
+    <p class="title is-4">y rápida referencia de inmuebles en el páis.</p>
+    <a href="/mapa" class="button is-black">Consultar precios</a>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Home',
+  components: {
+  }
+}
+</script>
